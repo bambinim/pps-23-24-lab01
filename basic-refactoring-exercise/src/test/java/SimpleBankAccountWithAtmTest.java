df7bd6444dcc91baead1ef270ff99f82650a7052
Matteo Bambini <matteo.bambini00@gmail.com>
2024-02-26 10:34:14 +0100

    Refactored SimpleBankAccountWithAtmTest for using transaction fee variable

--- basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
@@ -26,27 +26,27 @@ public class SimpleBankAccountWithAtmTest {
     @Test
     void testDeposit() {
         bankAccount.deposit(accountHolder.getId(), 100);
-        assertEquals(99, bankAccount.getBalance());
+        assertEquals(100 - SimpleBankAccountWithAtm.TRANSACTION_FEE, bankAccount.getBalance());
     }
 
     @Test
     void testWrongDeposit() {
         bankAccount.deposit(accountHolder.getId(), 100);
         bankAccount.deposit(2, 50);
-        assertEquals(99, bankAccount.getBalance());
+        assertEquals(100 - SimpleBankAccountWithAtm.TRANSACTION_FEE, bankAccount.getBalance());
     }
 
     @Test
     void testWithdraw() {
         bankAccount.deposit(accountHolder.getId(), 100);
         bankAccount.withdraw(accountHolder.getId(), 70);
-        assertEquals(28, bankAccount.getBalance());
+        assertEquals(30 - 2 * SimpleBankAccountWithAtm.TRANSACTION_FEE, bankAccount.getBalance());
     }
 
     @Test
     void testWrongWithdraw() {
         bankAccount.deposit(accountHolder.getId(), 100);
         bankAccount.withdraw(2, 70);
-        assertEquals(99, bankAccount.getBalance());
+        assertEquals(100 - SimpleBankAccountWithAtm.TRANSACTION_FEE, bankAccount.getBalance());
     }
 }
