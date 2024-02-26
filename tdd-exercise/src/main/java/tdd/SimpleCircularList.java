66fb05cdfe8e16ae0fa36c712f36789d8614e88e
Matteo Bambini <matteo.bambini00@gmail.com>
2024-02-26 10:44:33 +0100

    Created SimpleCircularList and implemented initially empty test

---
+++ tdd-exercise/src/main/java/tdd/SimpleCircularList.java
@@ -0,0 +1,43 @@
+package tdd;
+
+import java.util.Optional;
+
+public class SimpleCircularList implements CircularList {
+
+    @Override
+    public void add(int element) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        // TODO Auto-generated method stub
+        return Optional.empty();
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
--- tdd-exercise/src/test/java/CircularListTest.java
+++ tdd-exercise/src/test/java/CircularListTest.java
@@ -1,17 +1,28 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import tdd.CircularList;
+import tdd.SimpleCircularList;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList list;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach() {
+        this.list = new SimpleCircularList();
     }
 
+    @Test
+    void testInitiallyEmpty() {
+        assertTrue(list.isEmpty());
+    }
 }
